71c3de264ed04e3aaa1940d59388f7af850e2350
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2021-11-03 16:54:15 -0300

    Function girarCarta OK, falta passar carta p o js

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let HTMLTemporario
 while (numeroCartas < 4 || numeroCartas > 14 || numeroCartas % 2 != 0) {
   numeroCartas = prompt('Digite o numero de cartas: ')
 }
-for (var i = 0; i < numeroCartas - 4; i++) {
+for (var i = 0; i < numeroCartas - 1; i++) {
   var elementoOriginal = document.querySelector('.carta')
   var elementoClone = elementoOriginal.cloneNode(true)
   const addCarta = document.querySelector('.container')
   // inserindo o elemento na paǵina
   addCarta.appendChild(elementoClone)
+}
+
+function girarCarta() {
+  let girarFront = document.querySelector('.front-face')
+  girarFront.classList.toggle('girarFront')
+  let girarBack = document.querySelector('.back-face')
+  girarBack.classList.toggle('girarBack')
 }
